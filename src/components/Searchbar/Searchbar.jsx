02d03f991024bc0fe0c3466f8b373e88dd1e7de0
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-09 23:50:53 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,10 +1,7 @@
-const { render } = require("@testing-library/react");
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '31629453-3aa42bb9ff6dc8c3c3e379cd8';
 
 const SearchBar = ({onSubmit}) => {
-    render(
+    return(
         <header class="searchbar">
             <form class="form">
                 <button type="submit" class="button">
