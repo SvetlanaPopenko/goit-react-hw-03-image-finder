ed3d09b30e45a34bbae4edb5eb8af505170436a7
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-12 00:08:13 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,22 +1,27 @@
+import { SearchbarHeader, SearchForm, SearchFormButton,SearchFormLabel, SearchFormInput } from "./Searchbar.styled";
 
-
-const SearchBar = ({onSubmit}) => {
+const SearchBar = ({ onSubmit }) => {
+    const handleSubmit = evt => {
+        evt.preventDefault();
+        onSubmit(evt.target.serch.value.trim())
+        evt.currentTarget.reset();
+    }
     return(
-        <header class="searchbar">
-            <form class="form">
-                <button type="submit" class="button">
-                    <span class="button-label">Search</span>
-                </button>
+        <SearchbarHeader>
+            <SearchForm onSubmit={handleSubmit}>
+                <SearchFormButton type="submit">
+                    <SearchFormLabel>Search</SearchFormLabel>
+                </SearchFormButton>
 
-                <input
-                    class="input"
+                <SearchFormInput
+                    name="search"
                     type="text"
                     autocomplete="off"
                     autofocus
                     placeholder="Search images and photos"
                 />
-            </form>
-        </header>
+            </SearchForm>
+        </SearchbarHeader>
     )
 };
 
