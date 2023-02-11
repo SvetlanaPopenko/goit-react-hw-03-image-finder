8ca5fc029d64dc1ee6f044cbcd22ef83b05534d5
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-12 00:34:15 +0200

    Update Modal.styled.jsx

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-.Overlay {
+export const Overlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
@@ -11,9 +11,9 @@ import styled from '@emotion/styled';
   align-items: center;
   background-color: rgba(0, 0, 0, 0.8);
   z-index: 1200;
-}
+`;
 
-.Modal {
+export const Modal = styled.div`
   max-width: calc(100vw - 48px);
   max-height: calc(100vh - 24px);
-}
+`;
