379283b9a953bff6f31bacd9c9e8718ce998711b
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-12 00:08:04 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
 import { Component } from "react";
+import SearchBar from "./Searchbar";
 // import { fetchPhotoApi } from "./Api/Api";
 
 
-export class App  extends Component {
-  async componentDidMount() {
-    fetch()
+export class App extends Component {
+  state = {
+    images: [],
+    searchQuery: '',
+    isLoading:false,
+
   }
+  handleFormSubmit = searchQuery => {
+    if (!searchQuery || searchQuery === this.setState.searchQuery) {
+      return;
+    }
+    this.setState({
+      isLoading:true,
+      searchQuery,
+      images:[],
+      })
+  };
   
   render() {
    return (
@@ -18,7 +32,8 @@ export class App  extends Component {
         fontSize: 40,
         color: '#010101'
       }}
-    >
+     >
+       <SearchBar onSubmit={this.handleFormSubmit}/>
         </div>
   );
 }
