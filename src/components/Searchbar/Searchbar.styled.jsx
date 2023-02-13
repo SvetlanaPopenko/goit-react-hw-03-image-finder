6637b09da392e11c5fd5ccf8cf045c87e7d33fba
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-13 14:51:25 +0200

    Update Searchbar.styled.jsx

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -1,7 +1,7 @@
- import styled from '@emotion/styled';
+import styled from '@emotion/styled';
 
 export const SearchbarHeader = styled.header`
-top: 0;
+  top: 0;
   left: 0;
   position: sticky;
   z-index: 1100;
@@ -16,22 +16,21 @@ top: 0;
   color: #fff;
   background-color: #3f51b5;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
-    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);` 
-  
-
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+`;
 
 export const SearchForm = styled.form`
-display: flex;
+  display: flex;
   align-items: center;
   width: 100%;
   max-width: 600px;
   background-color: #fff;
   border-radius: 3px;
-  overflow: hidden;` 
-  
+  overflow: hidden;
+`;
 
-
-export const  SearchFormButton=styled.button`display: inline-block;
+export const SearchFormButton = styled.button`
+  display: inline-block;
   width: 48px;
   height: 48px;
   border: 0;
@@ -43,21 +42,23 @@ export const  SearchFormButton=styled.button`display: inline-block;
   transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
   cursor: pointer;
   outline: none;
-  :hover{
-opacity: 1;
+  :hover {
+    opacity: 1;
   }
-  `
-  
-export const SearchFormLabel=styled.span`position: absolute;
+`;
+
+export const SearchFormLabel = styled.span`
+  position: absolute;
   width: 1px;
   height: 1px;
   padding: 0;
   overflow: hidden;
   clip: rect(0, 0, 0, 0);
   white-space: nowrap;
-  clip-path: inset(50%);` 
+  clip-path: inset(50%);
+`;
 
-export const SearchFormInput=styled.input` 
+export const SearchFormInput = styled.input`
   display: inline-block;
   width: 100%;
   font: inherit;
@@ -66,8 +67,8 @@ export const SearchFormInput=styled.input`
   outline: none;
   padding-left: 4px;
   padding-right: 4px;
-::placeholder {
-  font: inherit;
-  font-size: 18px;}
-`
-  
+  ::placeholder {
+    font: inherit;
+    font-size: 18px;
+  }
+`;
