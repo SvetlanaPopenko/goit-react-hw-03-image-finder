1560b479fa683b71fa4172d3cb74c17bf60891d5
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-13 13:22:10 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,28 +1,36 @@
-import { SearchbarHeader, SearchForm, SearchFormButton,SearchFormLabel, SearchFormInput } from "./Searchbar.styled";
+import {
+  SearchbarHeader,
+  SearchForm,
+  SearchFormButton,
+  SearchFormLabel,
+  SearchFormInput,
+} from './Searchbar.styled';
+import { BsSearch } from 'react-icons/bs';
 
 const SearchBar = ({ onSubmit }) => {
-    const handleSubmit = evt => {
-        evt.preventDefault();
-        onSubmit(evt.target.serch.value.trim())
-        evt.currentTarget.reset();
-    }
-    return(
-        <SearchbarHeader>
-            <SearchForm onSubmit={handleSubmit}>
-                <SearchFormButton type="submit">
-                    <SearchFormLabel>Search</SearchFormLabel>
-                </SearchFormButton>
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    onSubmit(evt.target.search.value.trim());
+    evt.currentTarget.reset();
+  };
+  return (
+    <SearchbarHeader>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchFormButton type="submit">
+          <BsSearch width={60} height={60} />
+          <SearchFormLabel>Search</SearchFormLabel>
+        </SearchFormButton>
 
-                <SearchFormInput
-                    name="search"
-                    type="text"
-                    autocomplete="off"
-                    autofocus
-                    placeholder="Search images and photos"
-                />
-            </SearchForm>
-        </SearchbarHeader>
-    )
+        <SearchFormInput
+          name="search"
+          type="text"
+          autocomplete="off"
+          
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </SearchbarHeader>
+  );
 };
 
 export default SearchBar;
