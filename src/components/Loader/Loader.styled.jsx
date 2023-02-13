4ead16199af592e929059643ed8dc8640140c030
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-13 14:42:06 +0200

    Update Loader.styled.jsx

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -1,7 +1,13 @@
 import styled from '@emotion/styled';
 
-export const Loading=styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-`
+export const Loading = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #000000;
+`;
